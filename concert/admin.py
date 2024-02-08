9f382b35fbb3270648223fa045a6083cb0618a89
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-02-08 17:19:39 +0100

    implemented django application

--- concert/admin.py
+++ concert/admin.py
@@ -3,14 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Concert
 from .models import ConcertAttending
-# from .models import Photo
-# from .models import Song
 
 
 admin.site.register(Concert)
 admin.site.register(ConcertAttending)
-#admin.site.register(Photo)
-#admin.site.register(Song)
+
 
 
 
